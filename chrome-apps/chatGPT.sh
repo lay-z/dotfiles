707ae1236b57006735c005fd648fc20bbbbdb156
Priyav Shah <priyavrocks@gmail.com>
2023-03-03 11:49:25 +0300

    modified sway config updated

---
+++ chrome-apps/chatGPT.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+google-chrome --force-device-scale-factor=1.0 --user-data-dir=$HOME/.config/google-chrome-for-chatgpt/ --app=https://chat.openai.com
--- sway/scripts/sway-select-window.sh
+++ sway/scripts/sway-select-window.sh
@@ -1,25 +1,32 @@
-#!/usr/bin/env bash
+#!/usr/bin/env python3
 
-swaymsg -t get_tree | jq -r '
-        # descend to workspace or scratchpad
-        .nodes[].nodes[]
-        # save workspace name as .w
-        | {"w": .name} + (
-                if .nodes then # workspace
-                        [recurse(.nodes[])]
-                else # scratchpad
-                        []
-                end
-                + .floating_nodes
-                | .[]
-                # select nodes with no children (windows)
-                | select(.nodes==[])
-        )
-        | ((.id | tostring) + "\t "
-        # remove markup and index from workspace name, replace scratch with "[S]"
-        + (.w | gsub("^[^:]*:|<[^>]*>"; "") | sub("__i3_scratch"; "[S]"))
-        + "\t " +  .name)
-        ' | wofi --show dmenu --prompt='Focus a window' | {
-    read -r id name
-    swaymsg "[con_id=$id]" focus
-}
+import subprocess
+
+
+result = subprocess.run("swaymsg", "-t", "get_tree", capture_output=True, text=True)
+
+print(result.stdout)
+
+# swaymsg -t get_tree | jq -r '
+#         # descend to workspace or scratchpad
+#         .nodes[].nodes[]
+#         # save workspace name as .w
+#         | {"w": .name} + (
+#                 if .nodes then # workspace
+#                         [recurse(.nodes[])]
+#                 else # scratchpad
+#                         []
+#                 end
+#                 + .floating_nodes
+#                 | .[]
+#                 # select nodes with no children (windows)
+#                 | select(.nodes==[])
+#         )
+#         | ((.id | tostring) + "\t "
+#         # remove markup and index from workspace name, replace scratch with "[S]"
+#         + "\t " +  .name)
+#         + (.w | gsub("^[^:]*:|<[^>]*>"; "") | sub("__i3_scratch"; "[S]"))
+#         ' | wofi --show dmenu --prompt='Focus a window' | {
+#     read -r id name
+#     swaymsg "[con_id=$id]" focus
+# }
