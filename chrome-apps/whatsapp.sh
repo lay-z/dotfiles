3768b8550ce7c441883beee66c6e0df6c98600a4
Priyav Shah <priyavrocks@gmail.com>
2023-11-02 13:59:58 +0000

    updated whatsapp to include wayland specific args if connecting to wayland session

--- chrome-apps/whatsapp.sh
+++ chrome-apps/whatsapp.sh
@@ -1,2 +1,14 @@
+#TODO work out if we can have one script to run all chrome apps from here
 #!/bin/sh
-google-chrome-stable --force-device-scale-factor=1.0 --user-data-dir=$HOME/.config/google-chrome-stable-for-whatsapp/ --app=https://web.whatsapp.com
+if [ -z "$XDG_SESSION_TYPE" ]; then
+  SESSION_TYPE=$(loginctl show-session $(loginctl | grep $(whoami) | awk '{print $1}') -p Type --value)
+else
+  SESSION_TYPE=$XDG_SESSION_TYPE
+fi
+
+if [[ "$SESSION_TYPE" == "wayland" ]]; then
+  # TODO figure out if we can just add wayland flags and then open them up
+  google-chrome-stable --enable-features=UseOzonePlatform --ozone-platform=wayland --force-device-scale-factor=1.5 --user-data-dir=$HOME/.config/google-chrome-stable-for-whatsapp/ --app=https://web.whatsapp.com
+else
+  google-chrome-stable --force-device-scale-factor=1.5 --user-data-dir=$HOME/.config/google-chrome-stable-for-whatsapp/ --app=https://web.whatsapp.com
+fi
