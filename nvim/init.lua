a35e8b71051487f7fae7707b7e797ebe1890f090
Priyav Shah <priyavrocks@gmail.com>
2024-07-30 22:22:42 +0100

    works with vscode nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,8 @@
 -- plugins needs to be first, since installed all plugins which will be used later
 require("layz.core.keymaps")
-if
-	vim.g.vscode
+if vim.g.vscode then
 	-- Only things for when running neovim in the vscode extension comes here
-then
+else
 	require("layz.lazy")
 	require("layz.core.tests")
 end
