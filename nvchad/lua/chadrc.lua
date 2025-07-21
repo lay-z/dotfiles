22da6d452acad922bc05313a0e26115debb3370f
layz <priyav.shah@protonmail.com>
2025-07-21 18:50:05 +0100

    feat: colors back in. Trying to figure out how to adjust nvchad to follow light and dark preferences. Kitty moved out of hypr

--- nvchad/lua/chadrc.lua
+++ nvchad/lua/chadrc.lua
@@ -6,10 +6,24 @@
 local M = {}
 
 M.base46 = {
-  theme = "ayu_light",
-  theme_toggle = { "ayu_light", "everforest_light" },
+  theme = "catppuccin",
   transparency = false
 }
+
+local terminal_mode_path = "/home/layz/.local/state/TERMINAL_MODE"
+local terminal_mode = nil
+
+local file = io.open(terminal_mode_path, "r")
+print("Terminal mode file found: " .. tostring(file ~= nil))
+if file then
+  terminal_mode = file:read("*l")
+  file:close()
+end
+
+print("Terminal mode: " .. terminal_mode)
+if terminal_mode == "light" then
+  M.base46.theme = "ayu_light"
+end
 
 M.ui = {
   telescope = {
